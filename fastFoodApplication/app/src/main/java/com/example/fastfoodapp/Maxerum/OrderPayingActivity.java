629b9137af40182e91890aba79f6a5d2069ec2e5
Максим Иващенко 850505 <ivas.maxon2109@gmail.com>
2021-02-12 13:20:06 +0300

    Add OrderPayingActivity

---
+++ fastFoodApplication/app/src/main/java/com/example/fastfoodapp/Maxerum/OrderPayingActivity.java
@@ -0,0 +1,14 @@
+package com.example.fastfoodapp.Maxerum;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class OrderPayingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_order_paying);
+    }
+}
