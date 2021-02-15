f39d0a6b272d7e4615935b31c5e9af1d552ba80b
_________Madara <dimas_orlov99@mail.ru>
2021-02-15 03:40:54 +0300

    Created my folder and Folder for payPage

---
+++ fastFoodApplication/app/src/main/java/com/example/fastfoodapp/Dim4es/PayPage/PayMainActivity.java
@@ -0,0 +1,9 @@
+package com.example.fastfoodapp.Dim4es.PayPage;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PayMainActivity extends AppCompatActivity {
+
+}
