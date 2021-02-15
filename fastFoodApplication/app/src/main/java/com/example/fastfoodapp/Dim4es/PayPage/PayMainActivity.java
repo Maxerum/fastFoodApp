392638f6afa3a3c0d1da0dfbe4e0084184426702
_________Madara <dimas_orlov99@mail.ru>
2021-02-16 02:22:25 +0300

    Add Back Arrow click listener

--- fastFoodApplication/app/src/main/java/com/example/fastfoodapp/Dim4es/PayPage/PayMainActivity.java
+++ fastFoodApplication/app/src/main/java/com/example/fastfoodapp/Dim4es/PayPage/PayMainActivity.java
@@ -1,9 +1,31 @@
 package com.example.fastfoodapp.Dim4es.PayPage;
 
+
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
 
+import com.example.fastfoodapp.R;
 public class PayMainActivity extends AppCompatActivity {
+    ImageView arrowBack;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pay_main);
 
+        arrowBack = findViewById(R.id.arrowBack);
+        arrowBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Arrow back to return to previous page
+                Toast toast = Toast.makeText(PayMainActivity.this, "Back arrow clicked", Toast.LENGTH_SHORT);
+                toast.setGravity(Gravity.LEFT | Gravity.TOP, 20, 30);
+                toast.show();
+            }
+        });
+    }
 }
