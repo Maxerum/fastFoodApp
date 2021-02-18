8a24054ab1935e4d9c08a209ed49317364364f64
_________Madara <dimas_orlov99@mail.ru>
2021-02-18 22:43:21 +0300

    Add Click listeners to Buttons on the page (with log info)

--- fastFoodApplication/app/src/main/java/com/example/fastfoodapp/Dim4es/PayPage/PayMainActivity.java
+++ fastFoodApplication/app/src/main/java/com/example/fastfoodapp/Dim4es/PayPage/PayMainActivity.java
@@ -9,6 +9,7 @@ import android.view.View;
 import android.widget.Adapter;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.Spinner;
 import android.widget.TextView;
@@ -20,6 +21,9 @@ public class PayMainActivity extends AppCompatActivity {
     TextView textViewToChooseSpinner;
     Spinner spinnerRestaurant;
 
+    //Buttons
+    Button userCreditCard, addUserCard, finalSendButton;
+
     //some names to fill the void
     String[] restaurants = {"Ресторан0", "Ресторан1", "Ресторан2"};
 
@@ -28,12 +32,19 @@ public class PayMainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pay_main);
 
+        //define all buttons with id in xml
+        userCreditCard = findViewById(R.id.userCreditCard);
+        addUserCard = findViewById(R.id.addUserCard);
+        finalSendButton = findViewById(R.id.finalSendButton);
+
+
+        //define arrow in toolbar
         arrowBack = findViewById(R.id.arrowBack);
 
         //INfo about "Choose restaurant"
         textViewToChooseSpinner = findViewById(R.id.textViewToChooseSpinner);
 
-        //
+        //define spinner
         spinnerRestaurant = findViewById(R.id.restaurantSpinner);
         //spinner contains TextView (to choose)
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, R.layout.support_simple_spinner_dropdown_item, restaurants);
@@ -65,6 +76,43 @@ public class PayMainActivity extends AppCompatActivity {
                 toast.show();
             }
         });
+
+
+
+        //user credit card (which already exist), **mb show?**
+        userCreditCard.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast toast = Toast.makeText(PayMainActivity.this, "User credit card button clicked", Toast.LENGTH_SHORT);
+                toast.setGravity(Gravity.LEFT | Gravity.TOP, 20, 30);
+                toast.show();
+            }
+        });
+
+
+        //add user credit card (? new page with formatted input?)
+        addUserCard.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast toast = Toast.makeText(PayMainActivity.this, "Add user credit Card button clicked", Toast.LENGTH_SHORT);
+                toast.setGravity(Gravity.LEFT | Gravity.TOP, 20, 30);
+                toast.show();
+            }
+        });
+
+
+        // success or not, this order(new page mb?)
+        finalSendButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast toast = Toast.makeText(PayMainActivity.this, "Отправить в печь - button is clicked", Toast.LENGTH_SHORT);
+                toast.setGravity(Gravity.LEFT | Gravity.TOP, 20, 30);
+                toast.show();
+            }
+        });
+
+
+
     }
 
     private String onItemSelectedHandler(AdapterView<?> adapterView, View view, int position, long id){
