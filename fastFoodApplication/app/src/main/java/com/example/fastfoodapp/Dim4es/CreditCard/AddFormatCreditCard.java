ebaca071007226fb5f96311f726f5460676ce17f
__Madara <dimas_orlov99@mail.ru>
2021-03-01 21:02:48 +0300

    Init Credit Card Page

---
+++ fastFoodApplication/app/src/main/java/com/example/fastfoodapp/Dim4es/CreditCard/AddFormatCreditCard.java
@@ -0,0 +1,16 @@
+package com.example.fastfoodapp.Dim4es.CreditCard;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.fastfoodapp.R;
+
+public class AddFormatCreditCard extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_format_credit_card);
+    }
+}
