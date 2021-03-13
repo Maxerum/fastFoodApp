6c78ef427da34f789f789c26854baf7715181df4
Mxq <dimas_orlov99@mail.ru>
2021-03-14 02:27:48 +0300

    Add logic
    Add info to Choose date dialog: textView Listeners, two NumberPickers

--- fastFoodApplication/app/src/main/java/com/example/fastfoodapp/Dim4es/CreditCard/AddFormatCreditCard.java
+++ fastFoodApplication/app/src/main/java/com/example/fastfoodapp/Dim4es/CreditCard/AddFormatCreditCard.java
@@ -2,16 +2,25 @@ package com.example.fastfoodapp.Dim4es.CreditCard;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Dialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.NumberPicker;
+import android.widget.TextView;
+
 
 import com.example.fastfoodapp.Dim4es.PayPage.PayMainActivity;
 import com.example.fastfoodapp.R;
 
 public class AddFormatCreditCard extends AppCompatActivity {
-    ImageView arrowBackCreditCard;
+    ImageView arrowBackOnCreditCard, calendarIcon;
+    Dialog validCreditCardDialog;
+    String month, year;
+    TextView cardValidDate;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,19 +28,80 @@ public class AddFormatCreditCard extends AppCompatActivity {
 
 
         //define arrowBackCreditCard
-        arrowBackCreditCard = findViewById(R.id.arrowBackCreditCard);
-
-
+        arrowBackOnCreditCard = findViewById(R.id.arrowBackOnCreditCardPage);
         //arrowBackCreditCard click listener
-        arrowBackCreditCard.setOnClickListener(new View.OnClickListener() {
+        arrowBackOnCreditCard.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
                 //back to previous page(pay page)
                 Intent intent = new Intent(AddFormatCreditCard.this, PayMainActivity.class);
                 startActivity(intent);
             }
         });
 
+
+        //define calendarIcon
+        calendarIcon = findViewById(R.id.chooseDateImageView);
+        //calendar click listener
+        calendarIcon.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showValidCreditCardDialog();
+            }
+        });
+
+    }
+
+    private void showValidCreditCardDialog() {
+        validCreditCardDialog = new Dialog(this); // create Dialog obj
+        //set our dialog layout
+        validCreditCardDialog.setContentView(R.layout.dim4es_dialog_to_choose_date);
+        validCreditCardDialog.setCancelable(false);
+
+        ////////////
+        //define NumberPickers for month and year
+        NumberPicker monthPicker, yearPicker;
+        ///////////
+        //month
+        monthPicker = validCreditCardDialog.findViewById(R.id.monthPicker);
+        monthPicker.setMinValue(1);
+        monthPicker.setMaxValue(12);
+        monthPicker.setValue(5);
+        ///////////
+        //year
+        yearPicker = validCreditCardDialog.findViewById(R.id.yearPicker);
+        yearPicker.setMinValue(21);
+        yearPicker.setMaxValue(77);
+        yearPicker.setValue(22);
+
+        ///////////////////////////////
+        //Define done and cancel textViews
+        TextView doneBtn, cancelBtn;
+        doneBtn = validCreditCardDialog.findViewById(R.id.textDone);
+        cancelBtn = validCreditCardDialog.findViewById(R.id.textCancel);
+        ///////////////////////////////
+        //add textView listeners
+        //done text
+        doneBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                month = "" + monthPicker.getValue();
+                year = "" + yearPicker.getValue();
+                //edit date in main page
+                cardValidDate = findViewById(R.id.cardValidDate);
+                cardValidDate.setText(month + "/" + year);
+                validCreditCardDialog.dismiss();
+            }
+        });
+        //cancel text
+        cancelBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                validCreditCardDialog.dismiss();
+            }
+        });
+
+        //show dialog
+        validCreditCardDialog.show();
     }
 }
